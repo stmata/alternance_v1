34af4fc56a5e3af71dc5f69fd648153aaa802ffa
stmata <108939392+stmata@users.noreply.github.com>
2024-10-14 23:36:57 -0400

    changement endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import PlatformPage from './components/PlatformPage/PlatformPage';
 import { AppProvider } from './AppContext';
 import SmartFilter from './components/SmartFilter/SmartFilter';
 import RstMatching from './components/RstMatching/RstMatching';
-import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   const location = useLocation(); // Get the current location
@@ -13,10 +12,10 @@ function App() {
   return (
     <AppProvider>
       {/* Conditionally render the Navbar only when the path is not '/' */}
-      {location.pathname !== '/' && <Navbar />}
+      <Navbar />
+      {/* {location.pathname !== '/' && <Navbar />} */}
       <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/PlatformPage" element={<PlatformPage />} />
+        <Route path="/" element={<PlatformPage />} />
         <Route path="/SmartMatching" element={<SmartFilter />} />
         <Route path="/rslts00" element={<RstMatching />} />
       </Routes>
